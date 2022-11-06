03a65efc2046b1f02500bc04f3de3b5e46551c61
adithjosh <ajoshg24@gmail.com>
2022-11-06 17:58:23 -0500

    Completed Customer.java

--- Customer.java
+++ Customer.java
@@ -1,10 +1,28 @@
 import java.io.*;
+import java.util.ArrayList;
 
 public class Customer {
     private Database db;
-    public Customer() {
+    String name;
+    public Customer(String name) {
         db = new Database("UserDatabase.txt");
+        this.name = name;
     }
-
+    public void viewSellers() throws InvalidUserException {
+        MessageManager manager = new MessageManager();
+        ArrayList<String> sellers = manager.getNames("Seller");
+        for (String seller: sellers) {
+            System.out.println(seller);
+        }
+    }
+    public void selectSeller(String username, String message) throws InvalidUserException {
+        MessageManager manager = new MessageManager();
+        ArrayList<String> sellers = manager.getNames("Seller");
+        for (String seller: sellers) {
+            if (seller.equals(username)) {
+                manager.messageUser(this.name, username, message);
+            }
+        }
+    }
 
 }
