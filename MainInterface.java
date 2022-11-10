e44224bcae45203340ab34cb168ea55d5a2fb1db
adithjosh <ajoshg24@gmail.com>
2022-11-10 16:24:42 -0500

    Implemented Database.java into login, creation, and deletion of account.

--- MainInterface.java
+++ MainInterface.java
@@ -1,12 +1,15 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class MainInterface {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidUserException {
+        Database db = new Database("Database.txt");
         String PROMPT = "Would you like to..." +
         "\n1. Login" +
-        "\n2. Create an account" + 
-        "\n3. Exit\n";
+        "\n2. Create an account" +
+                "\n3. Delete an account" +
+        "\n4. Exit\n";
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to the {**NAME**} application");
@@ -16,7 +19,7 @@ public class MainInterface {
             try {
                 resp = scan.nextInt();
                 scan.nextLine();
-                if (resp < 1 || resp > 3) {
+                if (resp < 1 || resp > 4) {
                     System.out.println("Input invalid, try again.");
                     System.out.println(PROMPT);
                     resp = -1;
@@ -27,18 +30,28 @@ public class MainInterface {
             }
         } while (resp == -1);
 
-        MessageManager messageManager = new MessageManager();
+        MessageManager messageManager = new MessageManager("UserDatabase.txt");
         if (resp == 1) { //Login
-            System.out.println("Please enter your username: ");
-            String username = scan.nextLine();
-            System.out.println("Please enter your password: ");
-            String password = scan.nextLine();
-
+            boolean loggedIn = false;
+            String email = "";
+            String password = "";
+            while (!loggedIn) {
+                System.out.println("Please enter your email: ");
+                email = scan.nextLine();
+                System.out.println("Please enter your password: ");
+                password = scan.nextLine();
+                if (db.verify(email, password)) {
+                    loggedIn = true;
+                    System.out.println("Login successful!");
+                    HashMap<String, String> acct = db.get("role", email);
+                    User user = new User(email, password, acct.get("email"));
+                } else {
+                    System.out.println("Incorrect username or password. Please try again");
+                }
+            }
             //incorporate Database
 
         } else if (resp == 2) { //Creating Acct
-            System.out.println("Please enter your username: ");
-            String username = scan.nextLine();
             System.out.println("Please enter your email: ");
             String email = scan.nextLine();
             System.out.println("Please enter your password: ");
@@ -54,10 +67,16 @@ public class MainInterface {
                     roleValid = true;
                 }
             }
-            User account = new User(username, email, password, role);
-
+            User account = new User(email, password, role);
+            db.add(email, password, Role.valueOf(role));
+            System.out.println("Account created!");
             //incorporate database
         } else if (resp == 3) {
+            System.out.println("Please enter your email: ");
+            String email = scan.nextLine();
+            db.remove(email);
+            System.out.println("Account deleted!");
+        } else if (resp == 4) {
             System.out.println("Have a nice rest of your day!");
         }
     }
