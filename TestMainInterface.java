845085b7a8afae3f6df5fd150b1799bb6e19413c
CyrilSharma <sharma.cyril@gmail.com>
2022-11-13 15:47:25 -0500

    added template

--- TestMainInterface.java
+++ TestMainInterface.java
@@ -1,3 +1,21 @@
-public class TestMainInterface {
-    
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TestMessageManager {
+    // Useful constants.
+    private static final String dbPath = "testMainInterface/databases/db.txt";
+    private static final String histPath = "testMainInterface/history";
+    private static final String inputPath = "testMainInterface/inputs";
+    private static final String outputPath = "testMainInterface/outputs";
+    private static ArrayList<String> messageIDs = new ArrayList<String>();
+    public static void main(String[] args) {
+        System.out.println("Test " + (test0() ? "Passed" : "Failed"));
+        String[] args2 = new String[] {"/bin/bash", "-c", "java MainInterface < testcase.txt", "with", "args"};
+        Process proc = new ProcessBuilder(args2).start();
+    }
+    public static boolean test0() {
+        // tests construction.
+        
+    }
 }
