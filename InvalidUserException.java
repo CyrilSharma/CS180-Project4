c6070e3c5d002faa460dbde3f3eb68a05fa1f452
Atharva Gupta <atharva.gu@gmail.com>
2022-11-13 21:14:05 -0500

    Added and Finalized Comments on the exception classes

--- InvalidUserException.java
+++ InvalidUserException.java
@@ -1,3 +1,13 @@
+/**
+ * Project 4 -> InvalidUserException
+ *
+ * class handles the exception where the user is invalid
+ *
+ * @author Atharva Gupta, Cyril Sharma, Josh George, Nitin Murthy, Jacob Choi, L11
+ *
+ * @version November 13, 2022
+ *
+ */
 public class InvalidUserException extends Exception {
     public InvalidUserException(String message) {
         super(message);
