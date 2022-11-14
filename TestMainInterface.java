f0ffc9f7b81ee922c6e6d84a35a6ede886d581cc
CyrilSharma <sharma.cyril@gmail.com>
2022-11-13 21:19:15 -0500

    add stuff

--- TestMainInterface.java
+++ TestMainInterface.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class TestMainInterface {
-    // Useful constants.
     public static void main(String[] args) throws IOException {
         File dir = new File("testMainInterface/inputs");
         File[] files = dir.listFiles();
