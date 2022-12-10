33ac3f378b00b40b0c1f9384b05a7b74145d7a37
Atharva Gupta <atharva.gu@gmail.com>
2022-12-10 17:54:04 -0500

    Added comments to FilterInterfaceGUI.java

--- FilterInterfaceGUI.java
+++ FilterInterfaceGUI.java
@@ -1,13 +1,28 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
-
+/**
+ * Project 5 -> FilterInterfaceGUI
+ *
+ * FilterIntererface that connects to the Filter GUI
+ *
+ * @author Atharva Gupta, Cyril Sharma, Josh George, Nitin Murthy, Jacob Choi, L11
+ *
+ * @version December 10, 2022
+ *
+ */
 public class FilterInterfaceGUI {
     private Translator tr;
     private static boolean status;
     public FilterInterfaceGUI() {
         tr = new Translator();
     }
+
+    /**
+     * returns the words that are filtered
+     *
+     * @return ArrayList<String> words</String>
+     */
     public ArrayList<String> getWords() {
         ArrayList<String> words = new ArrayList<>();
         try {
@@ -17,6 +32,11 @@ public class FilterInterfaceGUI {
         }
         return words;
     }
+    /**
+     * adds a word to the query
+     *
+     * @param word
+     */
     public void addWord(String word) {
         try {
             String[] param = {word};
@@ -25,7 +45,11 @@ public class FilterInterfaceGUI {
 
         }
     }
-
+    /**
+     * removes a word to the query
+     *
+     * @param word
+     */
     public void removeWord(String word) throws InvalidWordException {
         try {
             String[] param = {word};
@@ -34,14 +58,28 @@ public class FilterInterfaceGUI {
             throw new InvalidWordException("No such word!");
         }
     }
+    /**
+     * changes the status of the boolean instance field variable
+     *
+     * @param stat
+     */
     public static void changeStatus(boolean stat) {
         status = stat;
     }
-
+    /**
+     * returns the status instance variable
+     *
+     * @return status
+     */
     public static boolean status() {
         return status;
     }
-
+    /**
+     * filters the message
+     *
+     * @param msg
+     * @return String filteredMessage
+     */
     public static String filterMsg(String msg) {
         Translator tr = new Translator();
         try {
