7912fb765d954a196513a485656d27388150cb19
Atharva Gupta <atharva.gu@gmail.com>
2022-12-10 17:13:22 -0500

    added comments to constant.java

--- Constants.java
+++ Constants.java
@@ -1,3 +1,13 @@
+/**
+ * Project 5 -> Constants
+ *
+ * Hosts the port number and the host name
+ *
+ * @author Atharva Gupta, Cyril Sharma, Josh George, Nitin Murthy, Jacob Choi, L11
+ *
+ * @version December 10, 2022
+ *
+ */
 public class Constants {
     static final int port = 3000;
     static final String hostname = "localhost";
