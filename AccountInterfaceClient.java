dc68fbd02b57d7067291350b68bc2aa8616d3795
Atharva Gupta <atharva.gu@gmail.com>
2022-12-10 16:52:52 -0500

    Added comments to the AccoutnInterfaceClient

--- AccountInterfaceClient.java
+++ AccountInterfaceClient.java
@@ -1,8 +1,24 @@
+/**
+ * Project 5 ->AccountInterfaceClient
+ *
+ * Interface class that connects with the AccountGUI.
+ *
+ * @author Atharva Gupta, Cyril Sharma, Josh George, Nitin Murthy, Jacob Choi, L11
+ *
+ * @version December 10, 2022
+ *
+ */
 public class AccountInterfaceClient {
     private Translator translator;
     public AccountInterfaceClient() {
         translator = new Translator();
     }
+
+    /**
+     * deletes the account of the user
+     *
+     * @param email email of the user
+     */
     public String deleteAccount(String email) throws Exception {
         String object = "Database";
         String function = "remove";
@@ -14,6 +30,12 @@ public class AccountInterfaceClient {
         return "SUCCESS";
     }
 
+    /**
+     * modifies the password of the user
+     *
+     * @param email email of user
+     * @param newPassword of the user
+     */
     public String modifyPassword(String email, String newPassword) throws Exception {
         String object = "Database";
         String function = "modify";
@@ -25,6 +47,12 @@ public class AccountInterfaceClient {
         return "SUCCESS";
     }
 
+    /**
+     * modifies the username
+     *
+     * @param email of the user
+     * @param newUsername of the user
+     */
     public String modifyUsername(String email, String newUsername) throws Exception {
         String object = "Database";
         String function = "modify";
@@ -36,6 +64,10 @@ public class AccountInterfaceClient {
         return "SUCCESS";
     }
 
+    /**
+     * logs the user out of the account
+     *
+     */
     public void logout() throws Exception {
         translator.query(new Query(null, "logout"));
     }
